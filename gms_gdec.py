8646b373121d3b552eaa7c90bff82ec4dc347146
Santiago Pestarini <santiagonob@gmail.com>
2012-09-16 14:13:29 -0300

    Agrega gms2gdel_list y asegura consistencia con gdec2gms_list.
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- gms_gdec.py
+++ gms_gdec.py
@@ -33,10 +33,20 @@ def gdec2gms(gdec):
 
 
 def gdec2gms_list(gdec_list):
-    """Conversion (g.gggggg) --> (gg,mm,ss.sssss) masiva.
+    """Conversion (g0.ggg,g1.ggg) --> ((g0,mm,ss.sss),(g1,mm,ss.sss)) masiva.
     Por ejemplo: gdec_list = [(-61.5,-31.456),(-63.456,31.3333)]"""
-    r = []
+    gms_list = []
     for i in gdec_list:
-        r.append((gdec2gms(i[0]), gdec2gms(i[1])))
-    return r
+        gms_list.append((gdec2gms(i[0]), gdec2gms(i[1])))
+    return gms_list
+
+
+def gms2gdec_list(gms_list):
+    """Conversion ((g0,mm,ss.sss),(g1,mm,ss.sss)) --> (g0.ggg,g1.ggg) masiva.
+    Por ejemplo:
+    gms_list = [((-61,30),(-31,45)),((-63,45),(31,20))]"""
+    gdec_list = []
+    for i in gms_list:
+        gdec_list.append((gms2gdec(*i[0]), gms2gdec(*i[1])))
+    return gdec_list
 
