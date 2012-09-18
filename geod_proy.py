a61eced701a74d4402dcb2664322482a59ed0c41
Santiago Pestarini <santiagonob@gmail.com>
2012-09-18 14:22:12 -0300

    Modificaciones grandes.
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- geod_proy.py
+++ geod_proy.py
@@ -1,51 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pyproj as pp
-import gms_gdec as gg
+import pyproj
+from gms_gyf import *
 
-
-def configurar_proyeccion(lat0, lon0, fesc=1, x0=0, y0=0, datum='WGS84'):
+def config_proy(latitud_de_origen, meridiano_central, falso_este=0,
+    falso_norte=0, factor_de_escala=1, datum='WGS84'):
     """Crea una proyeccion de tipo mercator transversal, con los siguientes
-    parametros, por ejemplo:
-      lat0 = gms(-31,44,30) Latitud de origen = 31º 44' 30" S
-      lon0 = gms(-61,14)    Meridiano de tangencia = 61º 14' 00" W
-      fesc = 1              Factor de escala (1 por defecto)
-      x0 = 0                Falso Norte (0 por defecto, en metros)
-      y0 = 0                Falso Este (0 por defecto, en metros)
-      datum = 'WGS84'       Datum (ver variable pj_ellps) (WGS84 por defecto)
-      proy = 'tmerc'        Tipo de proyección (ver pj_list) (tmerc por defecto)
+    parametros:
+      latitud_de_origen     Latitud de origen
+      meridiano_central     Meridiano central
+      falso_este            Falso Este (0 por defecto, en metros)
+      falso_norte           Falso Norte (0 por defecto, en metros)
+      factor_de_escala      Factor de escala (1 por defecto)
+      datum                 Datum, 'WGS84' por defecto (ver variable pj_ellps
+      para otros datums posibles, ej: 'GRS80')
     """
-    pmerct = pp.Proj(
+    pmerct = pyproj.Proj(
         proj='tmerc',
-        lat_0=lat0,
-        lon_0=lon0,
-        k=fesc,
-        x_0=x0,
-        y_0=y0,
+        lat_0=latitud_de_origen,
+        lon_0=meridiano_central,
+        k=factor_de_escala,
+        x_0=falso_norte,
+        y_0=falso_este,
         ellps=datum)
     return pmerct
 
 
-def proy2geod(coord_proy, proy):
-    coord_geod = []
-    for i in coord_proy:
-        coord_geod.append(proy(i[0], i[1], inverse=True))
-    return coord_geod
+def proy2geod(archivo_de_coordenadas, proyeccion, separador=','):
+    ext = '.geod'
+    geod_proy(archivo_de_coordenadas, proyeccion, ext, True, separador)
 
 
-# Otra opción: Al revés
-# Transformar de geodésicas a proyectadas
-# Cargar las coordenadas expresadas en geodésicas.
-# En pares (lon, lat):
-#   Pueden ser directamente en grados y fracción o en grados sexagesimales
-# Por ejemplo:
-#   coord_geod = [(gg.ggggº, gg.ggggº), (gg.ggggº, gg.ggggº), ...]
-#   ó
-#   coord_geod = [(gms(g0,mm,ss.sss), gms(g0,mm,ss.sss)), ...]
-def geod2proy(coord_geod, proy):
-    coord_proy = []
-    for i in coord_geod:
-        coord_proy.append(proy(i[0], i[1]))
-    return coord_proy
+def geod2proy(archivo_de_coordenadas, proyeccion, separador=','):
+    ext = '.proy'
+    geod_proy(archivo_de_coordenadas, proyeccion, ext, False, separador)
+
+
+def geod_proy(coord_file, proj, file_ext, inv, sep=','):
+    file_in = open(coord_file, 'r')
+    file_out = open(coord_file + file_ext, 'a')
+    for entry in file_in:
+      if not entry.startswith('#') and len(entry.strip()) != 0 \
+        and entry.find(sep) != -1:
+        coord_id, coord_in1, coord_in2 = entry.split(sep)
+        if not inv:
+          g1, m1, s1 = coord_in1.strip().split(' ')
+          g2, m2, s2 = coord_in2.strip().split(' ')
+          coord_in1 = gms2gyf(int(g1), int(m1), float(s1))
+          coord_in2 = gms2gyf(int(g2), int(m2), float(s2))
+        coord_out1, coord_out2 = proj(coord_in1, coord_in2, inverse=inv)
+        if inv:
+          g1, m1, s1 = gyf2gms(coord_out1)
+          g2, m2, s2 = gyf2gms(coord_out2)
+          coord_out1 = str(g1) + ' ' + str(m1) + ' ' + str(s1)
+          coord_out2 = str(g2) + ' ' + str(m2) + ' ' + str(s2)
+        file_out.write(str(coord_id) + sep + str(coord_out1) \
+          + sep + str(coord_out2) + '\n')
+    file_in.close()
+    file_out.close()
 
--- gms_gyf.py
+++ gms_gyf.py
@@ -7,46 +7,48 @@
 from numpy import sign, trunc, abs
 
 
-def gms2gdec(grados, minutos=0, segundos=0):
-    """Conversion (gg,mm,ss.sssss) --> (g.gggggg)
+def gms2gyf(grados, minutos=0, segundos=0):
+    """Conversion (gg,mm,ss.sssss) --> (g.frac)
     Por ejemplo:
-        gms2gdec(-61,30)          = -61.5
-        gms2gdec(-32,45,22.22222) = -32.xxxxxxxx
-        gms2gdec(-33)             = -33.0"""
+        gms2gyf(-61,30)  = -61.5
+        gms2gyf(0,-45)   = -0.75
+        gms2gyf(-33)     = -33.0"""
     s = sign(grados)
     signo = 1 if s == 0 else s
     return signo * (signo * grados + minutos / 60.0 + segundos / 3600.0)
 
 
-def gdec2gms(gdec):
-    """Conversion (g.gggggg) --> (gg,mm,ss.sssss)
-    Por ejemplo: gdec2gms(61.5) = (61,30)"""
+def gyf2gms(gyf):
+    """Conversion (g.frac) --> (gg,mm,ss.sssss)
+    Por ejemplo:
+        gyf2gms(-61.5) = (-61,30,0.0)
+        gyf2gms(-0.75) = (0,-45,0.0)"""
     # hacer las cuentas
-    g = trunc(gdec)
-    m = (gdec - g) * 60
+    g = trunc(gyf)
+    m = (gyf - g) * 60
     s = (m - trunc(m)) * 60
     # preparar la presentación
     g = int(g)
     m = int(m) if g == 0 else int(abs(m))
-    s = abs(s) if m != 0 else s
+    s = abs(s) if m != 0 and g != 0 else s
     return (g, m, s)
 
 
-def gdec2gms_list(gdec_list):
+def gyf2gms_list(gyf_list):
     """Conversion (g0.ggg,g1.ggg) --> ((g0,mm,ss.sss),(g1,mm,ss.sss)) masiva.
-    Por ejemplo: gdec_list = [(-61.5,-31.456),(-63.456,31.3333)]"""
+    Por ejemplo: gyf_list = [(-61.5,-31.456),(-63.456,31.3333),...]"""
     gms_list = []
-    for i in gdec_list:
-        gms_list.append((gdec2gms(i[0]), gdec2gms(i[1])))
+    for i in gyf_list:
+        gms_list.append((gyf2gms(i[0]), gyf2gms(i[1])))
     return gms_list
 
 
-def gms2gdec_list(gms_list):
+def gms2gyf_list(gms_list):
     """Conversion ((g0,mm,ss.sss),(g1,mm,ss.sss)) --> (g0.ggg,g1.ggg) masiva.
     Por ejemplo:
     gms_list = [((-61,30),(-31,45)),((-63,45),(31,20))]"""
-    gdec_list = []
+    gyf_list = []
     for i in gms_list:
-        gdec_list.append((gms2gdec(*i[0]), gms2gdec(*i[1])))
-    return gdec_list
+        gyf_list.append((gms2gyf(*i[0]), gms2gyf(*i[1])))
+    return gyf_list
 
